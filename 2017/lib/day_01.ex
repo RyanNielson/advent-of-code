fb9e54bd30c5e7ee4769be17070c130a84c3e978
Ryan Nielson <ryan.nielson@gmail.com>
2017-12-01 11:42:20 -0400

    Improved solution and finished part 2

--- 2017/lib/day_01.ex
+++ 2017/lib/day_01.ex
@@ -1,26 +1,38 @@
 defmodule AdventOfCode2017.Day01 do
-  def run(input) do
+  def part1(input) do
     input 
-    |> parse_to_circular_list
-    |> combine_like_values
-    |> sum
+    |> parse_to_list
+    |> summable_list(1)
+    |> Enum.sum
   end
 
-  def parse_to_circular_list(input) do
-    first = input |> String.first
+  def part2(input) do
     input 
-    |> Kernel.<>(first)
+    |> parse_to_list
+    |> summable_list
+    |> Enum.sum
+  end
+
+  defp parse_to_list(input) do
+    input 
     |> String.graphemes
     |> Enum.map(&String.to_integer/1)
   end
 
-  def combine_like_values(circular_list) do
-    circular_list
-    |> Enum.chunk_by(&(&1))
+  defp summable_list(list, offset) do
+    list 
+    |> Enum.with_index
+    |> Enum.filter(fn({num, index}) -> num == Enum.at(list, wrapped_index(length(list), index + offset)) end)
+    |> Enum.map(fn({num, _index}) -> num end)
   end
 
-  def sum(grouped_list) do
-    grouped_list
-    |> Enum.reduce(0, fn([num | rest], acc) -> acc + num * length(rest) end)
+  defp summable_list(list) do
+    offset = list |> length |> div(2)
+    list 
+    |> summable_list(offset)
+  end
+
+  defp wrapped_index(length, index) do
+    Kernel.rem(index, length)
   end
 end
--- 2017/test/day_01_test.exs
+++ 2017/test/day_01_test.exs
@@ -4,34 +4,35 @@ defmodule AdventOfCode2017.Day01Test do
   import AdventOfCode2017.Day01
   import AdventOfCode2017.TestHelper
 
-  test "run" do
-      assert run(input_sample_1()) == 3
-      assert run(input_sample_2()) == 4
-      assert run(input_sample_3()) == 0
-      assert run(input_sample_4()) == 9
-      assert run(input("test/input/day_01.txt")) == 1144
-  end
- 
-  test "parse input into circular list" do
-    assert parse_to_circular_list(input_sample_1()) == input_sample_1_circular_list()
-    assert parse_to_circular_list(input_sample_2()) == input_sample_2_circular_list()
-    assert parse_to_circular_list(input_sample_3()) == input_sample_3_circular_list()
-    assert parse_to_circular_list(input_sample_4()) == input_sample_4_circular_list()
-  end
-
-  test "combine like values" do
-    assert combine_like_values(input_sample_1_circular_list()) == [[1, 1], [2, 2], [1]]
-    assert combine_like_values(input_sample_2_circular_list()) == [[1, 1, 1, 1, 1]]
-    assert combine_like_values(input_sample_3_circular_list()) == [[1], [2], [3], [4], [1]]
-    assert combine_like_values(input_sample_4_circular_list()) == [[9], [1], [2], [1], [2], [1], [2], [9, 9]]
+  describe "Part 1" do
+    test "part1" do
+        assert part1(input_sample_1()) == 3
+        assert part1(input_sample_2()) == 4
+        assert part1(input_sample_3()) == 0
+        assert part1(input_sample_4()) == 9
+        assert part1(input("test/input/day_01.txt")) == 1144
+    end
+  
+    defp input_sample_1, do: "1122"
+    defp input_sample_2, do: "1111"
+    defp input_sample_3, do: "1234"
+    defp input_sample_4, do: "91212129"
   end
 
-  defp input_sample_1, do: "1122"
-  defp input_sample_2, do: "1111"
-  defp input_sample_3, do: "1234"
-  defp input_sample_4, do: "91212129"
-  defp input_sample_1_circular_list, do: [1, 1, 2, 2, 1]
-  defp input_sample_2_circular_list, do: [1, 1, 1, 1, 1]
-  defp input_sample_3_circular_list, do: [1, 2, 3, 4, 1]
-  defp input_sample_4_circular_list, do: [9, 1, 2, 1, 2, 1, 2, 9, 9]
+  describe "Part 2" do
+    test "part2" do
+      assert part2(input_sample_1_2()) == 6
+      assert part2(input_sample_2_2()) == 0
+      assert part2(input_sample_3_2()) == 4
+      assert part2(input_sample_4_2()) == 12
+      assert part2(input_sample_5_2()) == 4
+      assert part2(input("test/input/day_01.txt")) == 1194
+    end
+
+    defp input_sample_1_2, do: "1212"
+    defp input_sample_2_2, do: "1221"
+    defp input_sample_3_2, do: "123425"
+    defp input_sample_4_2, do: "123123"
+    defp input_sample_5_2, do: "12131415"
+  end
 end
