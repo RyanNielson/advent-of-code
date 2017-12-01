f226f94a9e43e98396aa0e2865f8b4e05030f2df
Ryan Nielson <ryan.nielson@gmail.com>
2017-12-01 16:16:43 -0400

    More cleanup

--- 2017/lib/day_01.ex
+++ 2017/lib/day_01.ex
@@ -1,23 +1,23 @@
 defmodule AdventOfCode2017.Day01 do
-  def part1(input), do: input |> parse_to_list |> summable_list(1) |> Enum.sum
+  def part1(input), do: input |> parse_to_list() |> summable_list(1) |> Enum.sum()
 
-  def part2(input), do: input |> parse_to_list |> summable_list |> Enum.sum
+  def part2(input), do: input |> parse_to_list() |> summable_list |> Enum.sum()
 
   defp parse_to_list(input) do
     input 
-    |> String.graphemes
+    |> String.graphemes()
     |> Enum.map(&String.to_integer/1)
   end
 
   defp summable_list(list, offset) do
     list 
-    |> Enum.with_index
+    |> Enum.with_index()
     |> Enum.filter(fn({num, index}) -> num == Enum.at(list, wrapped_index(length(list), index + offset)) end)
-    |> Enum.map(fn({num, _index}) -> num end)
+    |> Enum.map(&elem(&1, 0))
   end
 
   defp summable_list(list) do
-    offset = list |> length |> div(2)
+    offset = list |> length() |> div(2)
     summable_list(list, offset)
   end
 
