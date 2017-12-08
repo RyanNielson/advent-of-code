9bf0744fc5c95961159fc72720af0acb22d38933
Nielson, Ryan <rnielson@cvent.com>
2017-12-08 10:00:17 -0400

    Day 8 cleanup

--- 2017/lib/day_08.ex
+++ 2017/lib/day_08.ex
@@ -9,11 +9,11 @@ defmodule AdventOfCode2017.Day08 do
   def part2(input) do
     input
     |> parse()
-    |> fill_registers_get_history()
-    |> highest_ever()
+    |> fill_registers()
+    |> highest_value_ever()
   end
 
-  def parse(input) do
+  defp parse(input) do
     input
     |> String.split((~r/\r\n|\r|\n/), trim: true)
     |> Enum.map(&String.split()/1)
@@ -22,57 +22,30 @@ defmodule AdventOfCode2017.Day08 do
     end)
   end
 
-  def init_registers(instructions) do
-    instructions
-    |> Enum.reduce(%{}, fn([name | _], registers) ->
-      Map.put(registers, name, 0)
-    end)
+  defp init_registers(instructions), do: Map.new(instructions, fn [name | _] -> {name, 0} end)
+
+  defp fill_registers(instructions),  do: Enum.map_reduce(instructions, init_registers(instructions), &modify_register/2)
+
+  defp modify_register([name, op, amount | condition], registers) do
+    if condition(registers, condition) do
+      new_value = new_value(registers, name, op, amount)
+      {new_value, Map.put(registers, name, new_value)}
+    else
+      {registers[name], registers}
+    end
   end
 
-  def fill_registers(instructions) do
-    instructions
-    |> Enum.reduce(init_registers(instructions), fn ([name, op, amount | condition], registers) ->
-      case (condition(registers, condition)) do
-        true -> handle_operation(registers, name, op, amount)
-        false -> registers
-      end
-    end)
-  end
+  defp highest_value({_, registers}), do: registers |> Map.values() |> Enum.max()
 
-  def fill_registers_get_history(instructions) do
-    instructions
-    |> Enum.map_reduce(init_registers(instructions), fn ([name, op, amount | condition], registers) ->
-      case (condition(registers, condition)) do
-        true ->
-          value = new_value(registers, name, op, amount)
-          {value, handle_operation(registers, name, op, amount)}
-        false ->
-          {registers[name], registers}
-      end
-    end)
-  end
+  defp highest_value_ever({history, _}), do: Enum.max(history)
 
-  def highest_value(registers) do
-    registers
-    |> Map.values()
-    |> Enum.max()
-  end
+  defp condition(registers, [register, ">", amount]), do: registers[register] > amount
+  defp condition(registers, [register, "<", amount]), do: registers[register] < amount
+  defp condition(registers, [register, ">=", amount]), do: registers[register] >= amount
+  defp condition(registers, [register, "<=", amount]), do: registers[register] <= amount
+  defp condition(registers, [register, "==", amount]), do: registers[register] == amount
+  defp condition(registers, [register, "!=", amount]), do: registers[register] != amount
 
-  def highest_ever({history, _}) do
-    history
-    |> Enum.max()
-  end
-
-  def condition(registers, [register, ">", amount]), do: registers[register] > amount
-  def condition(registers, [register, "<", amount]), do: registers[register] < amount
-  def condition(registers, [register, ">=", amount]), do: registers[register] >= amount
-  def condition(registers, [register, "<=", amount]), do: registers[register] <= amount
-  def condition(registers, [register, "==", amount]), do: registers[register] == amount
-  def condition(registers, [register, "!=", amount]), do: registers[register] != amount
-
-  def handle_operation(registers, register, "inc", amount), do: Map.update(registers, register, 0, &(&1 + amount))
-  def handle_operation(registers, register, "dec", amount), do: Map.update(registers, register, 0, &(&1 - amount))
-
-  def new_value(registers, register, "inc", amount), do: registers[register] + amount
-  def new_value(registers, register, "dec", amount), do: registers[register] - amount
+  defp new_value(registers, register, "inc", amount), do: registers[register] + amount
+  defp new_value(registers, register, "dec", amount), do: registers[register] - amount
 end
--- 2017/test/day_08_test.exs
+++ 2017/test/day_08_test.exs
@@ -14,27 +14,10 @@ defmodule AdventOfCode2017.Day08Test do
     assert part2(input("test/input/day_08.txt")) == 3818
   end
 
-  test "parse" do
-    assert parse(sample_input_1()) == sample_instructions_1()
-  end
-
-  test "init registers" do
-    assert init_registers(sample_instructions_1()) == %{"b" => 0, "a" => 0, "c" => 0}
-  end
-
   defp sample_input_1 do
     "b inc 5 if a > 1
     a inc 1 if b < 5
     c dec -10 if a >= 1
     c inc -20 if c == 10"
-  end
-
-  defp sample_instructions_1 do
-    [
-      ["b", "inc", 5, "a", ">", 1],
-      ["a", "inc", 1, "b", "<", 5],
-      ["c", "dec", -10, "a", ">=", 1],
-      ["c", "inc", -20, "c", "==", 10]
-    ]
   end
 end
