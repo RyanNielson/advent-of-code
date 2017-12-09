63414fd46d218c51649a885d6feffddd5334ddc1
Ryan Nielson <ryan.nielson@gmail.com>
2017-12-09 15:03:02 -0400

    Day 9 cleanup

--- 2017/lib/day_09.ex
+++ 2017/lib/day_09.ex
@@ -1,47 +1,25 @@
 defmodule AdventOfCode2017.Day09 do
-  def part1(input) do
-    input
-    |> parse()
-    |> handle(1)
-  end
+  def part1(input), do: input |> group(1)
 
-  def part2(input) do
-    input
-    |> parse()
-    |> handle_2(0)
-  end
+  def part2(input), do: input |> group_2(0)
+  
+  defp group("{" <> rest, level), do: level + group(rest, level + 1)
+  defp group("}" <> rest, level), do: group(rest, level - 1)
+  defp group("<" <> rest, level), do: garbage(rest, level)
+  defp group(<<_::binary-size(1)>> <> rest, level), do: group(rest, level)
+  defp group("", _), do: 0
 
-  # TODO: Switch to pattern matching on strings instead.
-  defp handle(["{" | rest], level), do: level + handle(rest, level + 1)
-  defp handle(["}" | rest], level), do: handle(rest, level - 1)
-  defp handle(["<" | rest], level), do: garbage(rest, level)
-  defp handle([_ | rest], level), do: handle(rest, level)
-  defp handle([], _), do: 0
+  defp garbage("!" <> <<_::binary-size(1)>> <> rest, level), do: garbage(rest, level)
+  defp garbage(">" <> rest, level), do: group(rest, level)
+  defp garbage(<<_::binary-size(1)>> <> rest, level), do: garbage(rest, level)
 
-  defp garbage(["!", _ | rest], level), do: garbage(rest, level)
-  defp garbage([">" | rest], level), do: handle(rest, level)
-  defp garbage([_ | rest], level), do: garbage(rest, level)
+  defp group_2("{" <> rest, count), do: group_2(rest, count)
+  defp group_2("}" <> rest, count), do: group_2(rest, count)
+  defp group_2("<" <> rest, count), do: garbage_2(rest, count)
+  defp group_2(<<_::binary-size(1)>> <> rest, count), do: group_2(rest, count)
+  defp group_2("", count), do: count
 
-
-  defp handle_2(["{" | rest], garbage_count), do: handle_2(rest, garbage_count)
-  defp handle_2(["}" | rest], garbage_count), do: handle_2(rest, garbage_count)
-  defp handle_2(["<" | rest], garbage_count), do: garbage_2(rest, garbage_count)
-  defp handle_2([_ | rest], garbage_count), do: handle_2(rest, garbage_count)
-  defp handle_2([], garbage_count), do: garbage_count
-
-  defp garbage_2(["!", _ | rest], garbage_count), do: garbage_2(rest, garbage_count)
-  defp garbage_2([">" | rest], garbage_count), do: handle_2(rest, garbage_count)
-  defp garbage_2([_ | rest], garbage_count), do: garbage_2(rest, garbage_count + 1)
-
-  defp parse(input), do: input |> String.split("", trim: true)
-
-  # def tt(<<c::utf8>> <> rest) do
-  #   IO.inspect(c)
-  #   IO.inspect(rest)
-  # end
-
-  # def tt2(<<c::binary-size(1), rest::binary>>) do
-  #   IO.inspect(c)
-  #   IO.inspect(rest)
-  # end
+  defp garbage_2("!" <> <<_::binary-size(1)>> <> rest, count), do: garbage_2(rest, count)
+  defp garbage_2(">" <> rest, count), do: group_2(rest, count)
+  defp garbage_2(<<_::binary-size(1)>> <> rest, count), do: garbage_2(rest, count + 1)
 end
