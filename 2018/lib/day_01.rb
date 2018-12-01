fd8a6e30a84bd653bf37e248136af1c4f3eb5f86
Ryan Nielson <ryan.nielson@gmail.com>
2018-12-01 16:16:48 -0400

    Added day 1 solutions

--- 2018/lib/day_01.rb
+++ 2018/lib/day_01.rb
@@ -1,5 +1,23 @@
+require "set"
+
 class Day01
-  def something
-    "banana"
+  def part1(input)
+    split_numbers(input).sum
+  end
+
+  def part2(input)
+    frequency = 0
+    frequencies_reached = Set[0]
+
+    split_numbers(input).cycle do |number|
+      frequency += number
+      break unless frequencies_reached.add?(frequency)
+    end
+
+    frequency
+  end
+
+  def split_numbers(input)
+    input.split.map(&:to_i)
   end
 end
--- 2018/test/day_01_test.rb
+++ 2018/test/day_01_test.rb
@@ -2,13 +2,22 @@ require "test_helper"
 require "day_01"
 
 class Day01Test < Minitest::Test
-  def test_that_it_has_a_version_number
-    assert Day01.new.something == "banana"
+  def setup
+    @day_01 = Day01.new
   end
 
-  def test_it_does_something_useful
-    puts input("day_01.txt");
+  def test_part1
+    assert_equal 3, @day_01.part1("+1, +1, +1")
+    assert_equal 0, @day_01.part1("+1, +1, -2")
+    assert_equal -6, @day_01.part1("-1, -2, -3")
+    assert_equal 547, @day_01.part1(input("day_01.txt"))
+  end
 
-    assert false
+  def test_part2
+    assert_equal 0, @day_01.part2("+1, -1")
+    assert_equal 10, @day_01.part2("+3, +3, +4, -2, -4")
+    assert_equal 5, @day_01.part2("-6, +3, +8, +5, -6")
+    assert_equal 14, @day_01.part2("+7, +7, -2, -7, -4")
+    assert_equal 14, @day_01.part2(input("day_01.txt"))
   end
 end
