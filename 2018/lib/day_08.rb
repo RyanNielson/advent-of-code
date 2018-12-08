4b4177cb9d340ba4af77ac7086a2abbaf7ff9108
Ryan Nielson <ryan.nielson@gmail.com>
2018-12-08 15:23:34 -0400

    Day 8 solutions

--- 2018/lib/day_08.rb
+++ 2018/lib/day_08.rb
@@ -1,75 +1,32 @@
 class Day08
-  # def part1(input)
-  #   steps = related_steps(input)
-  #   result = []
+  def part1(input)
+    metadata_sum(numbers(input));
+  end
 
-  #   while !steps.empty? do
-  #     next_step = steps.keys.sort.find{ |letter| steps[letter].empty? }
+  def part2(input)
+    child_sums(numbers(input));
+  end
 
-  #     result << next_step
-  #     steps.delete(next_step)
-  #     steps.each { |key, dependencies| dependencies.delete(next_step) }
-  #   end
+  def metadata_sum(nums)
+    child_count, metadata_count = nums.shift(2)
 
-  #   result.join
-  # end
+    sum = child_count.times.map { metadata_sum(nums) }.sum
 
-  # def part2(input, worker_count, step_duration)
-  #   steps = related_steps(input)
-  #   workers = Array.new(worker_count, [0, nil])
-  #   done = []
-  #   total_time = 0;
+    sum + nums.shift(metadata_count).sum
+  end
 
-  #   0.step.each do |time|
-  #     available_steps = steps.keys.sort.select{ |letter| steps[letter].empty? }
-  #     available_workers = workers.each_with_index.select { |(t, letter), i| t <= 0 }
+  def child_sums(nums)
+    child_count, metadata_count = nums.shift(2)
 
-  #     available_steps.each_with_index do |step, i|
-  #       worker = available_workers[i]
-
-  #       unless worker.nil?
-  #         workers[worker[1]] = [step_time(step, step_duration), step] unless worker.nil?
-  #         steps.delete(step)
-  #       end
-  #     end
-
-  #     workers = workers.map do |t, letter|
-  #       t -= 1
-  #       if t <= 0 && !letter.nil?
-  #         done << letter
-  #         steps.each { |key, dependencies| dependencies.delete(letter) }
-  #         [0, nil]
-  #       else
-  #         [t, letter]
-  #       end
-  #     end
-
-  #     total_time = time + 1
-  #     busy_workers = workers.each_with_index.select { |(t, letter), i| t > 0 }
-  #     break if steps.empty? && busy_workers.empty?
-  #   end
-
-  #   total_time
-  # end
-
-
-  # def related_steps(input)
-  #   input.split("\n").map(&:strip).reduce(Hash.new { |h, k| h[k] = [] }) do |steps, line|
-  #     a, b = /Step (.) must be finished before step (.) can begin./.match(line).captures
-
-  #     steps[b] << a
-  #     steps[a] = [] if steps[a].empty?
-  #     steps
-  #   end
-  # end
-
-  # def step_time(letter, step_duration)
-  #   letter_value(letter) + step_duration
-  # end
-
-  # def letter_value(letter)
-  #   letter.ord - "A".ord + 1
-  # end
+    child_sums = child_count.times.map { child_sums(nums) }
+    metadata   = nums.shift(metadata_count)
+    
+    if child_count == 0
+      metadata.sum
+    else
+      metadata.map{ |i| child_sums[i - 1] }.compact.sum
+    end
+  end
 
   def numbers(input)
     input.split.map(&:to_i)
--- 2018/test/day_08_test.rb
+++ 2018/test/day_08_test.rb
@@ -6,32 +6,15 @@ class Day08Test < Minitest::Test
     @day_08 = Day08.new
   end
 
-  # def test_part1
-  #   assert_equal "CABDFE", @day_07.part1(sample_input_1)
-  #   assert_equal "BCADPVTJFZNRWXHEKSQLUYGMIO", @day_07.part1(input("day_07.txt"))
-  # end
+  def test_part_1
+    assert_equal 138, @day_08.part1(sample_input_1)
+    assert_equal 43996, @day_08.part1(input("day_08.txt"))
+  end
 
-  # def test_part2
-  #   assert_equal 15, @day_07.part2(sample_input_1, 2, 0)
-  #   assert_equal 973, @day_07.part2(input("day_07.txt"), 5, 60)
-  # end
-
-  # def test_letter_value
-  #   assert_equal 1, @day_07.letter_value("A")
-  #   assert_equal 2, @day_07.letter_value("B")
-  #   assert_equal 3, @day_07.letter_value("C")
-  #   assert_equal 26, @day_07.letter_value("Z")
-  # end
-
-  # def test_step_time
-  #   assert_equal 1, @day_07.step_time("A", 0)
-  #   assert_equal 26, @day_07.step_time("Z", 0)
-  #   assert_equal 86, @day_07.step_time("Z", 60)
-  # end
-
-  # def test_related_steps
-  #   assert_equal sample_input_1_steps_2, @day_07.related_steps(sample_input_1)
-  # end
+  def test_part_2
+    assert_equal 66, @day_08.part2(sample_input_1)
+    assert_equal 35189, @day_08.part2(input("day_08.txt"))
+  end
 
   def test_numbers
     assert_equal sample_input_1_numbers, @day_08.numbers(sample_input_1)
@@ -44,12 +27,4 @@ class Day08Test < Minitest::Test
   def sample_input_1_numbers
     [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
   end
-
-  # def sample_input_1_steps
-  #   {"A" => ["C"], "F" => ["C"], "B" => ["A"], "D" => ["A"], "E" => ["B", "D", "F"]}
-  # end
-
-  # def sample_input_1_steps_2
-  #   {"C" => [], "A" => ["C"], "F" => ["C"], "B" => ["A"], "D" => ["A"], "E" => ["B", "D", "F"]}
-  # end
 end
