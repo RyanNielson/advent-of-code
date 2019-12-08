636f96b537b981233d64f51e75ae22dd8d8ffc2d
Ryan Nielson <ryan.nielson@gmail.com>
2019-12-08 12:36:34 -0400

    Day 8 part 2 and code refactoring

--- 2019/lib/day_08.ex
+++ 2019/lib/day_08.ex
@@ -5,25 +5,40 @@ defmodule Day08 do
     2500
   """
   def part1(input, width, height) do
-    selected_layer =
+    target_layer =
       input
-      |> parse()
-      |> Enum.chunk_every(width * height)
+      |> layers(width, height)
       |> Enum.min_by(fn layer ->
         layer |> Enum.filter(&(&1 == 0)) |> Enum.count()
       end)
 
-    one_digits_count =
-      selected_layer
-      |> Enum.filter(&(&1 == 1))
-      |> Enum.count()
+    Enum.count(target_layer, &(&1 == 1)) * Enum.count(target_layer, &(&1 == 2))
+  end
 
-    two_digits_count =
-      selected_layer
-      |> Enum.filter(&(&1 == 2))
-      |> Enum.count()
+  @doc """
+  Prints the message stored in the input. In this case CYUAH should be output.
 
-    one_digits_count * two_digits_count
+  ## Examples
+    iex> Helpers.input("day_08") |> Day08.part2(25, 6)
+    :ok
+  """
+  def part2(input, width, height) do
+    input
+    |> layers(width, height)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(fn pixel_values ->
+      Enum.find(pixel_values, fn pixel -> pixel == 0 or pixel == 1 end)
+    end)
+    |> Enum.chunk_every(width)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.each(&IO.puts/1)
+  end
+
+  defp layers(input, width, height) do
+    input
+    |> parse()
+    |> Enum.chunk_every(width * height)
   end
 
   defp parse(input) do
