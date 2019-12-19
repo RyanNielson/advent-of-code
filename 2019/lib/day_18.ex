c1cca9405e357039baba08dc3594d88641c6a061
Ryan Nielson <ryan.nielson@gmail.com>
2019-12-19 15:08:42 -0400

    Speed up checks by just finding first matching for part2 start

--- 2019/lib/day_18.ex
+++ 2019/lib/day_18.ex
@@ -8,7 +8,6 @@ defmodule Day18 do
     input
     |> parse()
     |> points(50, 50)
-    |> Map.values()
     |> Enum.count(& &1)
   end
 
@@ -25,25 +24,19 @@ defmodule Day18 do
 
     {start_x, start_y} =
       for(x <- 50..99, y <- 50..99, do: {x, y})
-      |> Enum.filter(fn {x, y} -> check_point(x, y, state) end)
-      |> Enum.max()
+      |> Enum.find(fn {x, y} -> check_point(x, y, state) end)
 
     {x, y} = find_square(state, start_x, start_y)
 
     x * 10000 + y
   end
 
-
-  # This can just return a list, no need for the coordinates other than debugging.
   defp points(program, width, height) do
     state = program |> Intcode.init()
 
-    for x <- 0..(width - 1), y <- 0..(height - 1), into: %{} do
-      {{x, y}, check_point(x, y, state)}
-    end
+    for x <- 0..(width - 1), y <- 0..(height - 1), do: check_point(x, y, state)
   end
 
-  # 100 or 99 >=?
   defp find_square(state, x, y) do
     if check_point(x, y, state) do
       if check_point(x + 99, y - 99, state), do: {x, y - 99}, else: find_square(state, x, y + 1)
