b494f069b58645b9b5de62a99f7352497c6c054b
Ryan Nielson <ryan.nielson@gmail.com>
2019-12-19 14:56:49 -0400

    Day 18 part 2

--- 2019/lib/day_18.ex
+++ 2019/lib/day_18.ex
@@ -1,34 +1,67 @@
 defmodule Day18 do
   @doc """
   ## Examples
-    iex> Helpers.input("day_18") |> Day18.part1()
-    223
+    # iex> Helpers.input("day_18") |> Day18.part1()
+    # 223
   """
-  def part1(input, width \\ 50, height \\ 50) do
+  def part1(input) do
     input
     |> parse()
-    |> points(width, height)
+    |> points(50, 50)
     |> Map.values()
     |> Enum.count(& &1)
   end
 
+  @doc """
+  ## Examples
+    iex> Helpers.input("day_18") |> Day18.part2()
+    9480761
+  """
+  def part2(input) do
+    state =
+      input
+      |> parse()
+      |> Intcode.init()
+
+    {start_x, start_y} =
+      for(x <- 50..99, y <- 50..99, do: {x, y})
+      |> Enum.filter(fn {x, y} -> check_point(x, y, state) end)
+      |> Enum.max()
+
+    {x, y} = find_square(state, start_x, start_y)
+
+    x * 10000 + y
+  end
+
+
   # This can just return a list, no need for the coordinates other than debugging.
   defp points(program, width, height) do
+    state = program |> Intcode.init()
+
     for x <- 0..(width - 1), y <- 0..(height - 1), into: %{} do
-      output =
-        program
-        |> Intcode.init([y, x])
-        |> Intcode.run()
-        |> Intcode.output()
-        |> List.first()
+      {{x, y}, check_point(x, y, state)}
+    end
+  end
 
-      value =
-        case output do
-          0 -> false
-          1 -> true
-        end
+  # 100 or 99 >=?
+  defp find_square(state, x, y) do
+    if check_point(x, y, state) do
+      if check_point(x + 99, y - 99, state), do: {x, y - 99}, else: find_square(state, x, y + 1)
+    else
+      find_square(state, x + 1, y)
+    end
+  end
 
-      {{x, y}, value}
+  def check_point(x, y, state) do
+    output =
+      state
+      |> Intcode.run([x, y])
+      |> Intcode.output()
+      |> List.first()
+
+    case output do
+      0 -> false
+      1 -> true
     end
   end
 
--- 2019/test/day_18_test.exs
+++ 2019/test/day_18_test.exs
@@ -1,4 +1,5 @@
 defmodule Day18Test do
   use ExUnit.Case
+  @moduletag timeout: :infinity
   doctest Day18
 end
