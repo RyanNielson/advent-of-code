2951b5f2817bbf140d5fbfd5405da36ecb6d3320
Ryan Nielson <ryan.nielson@gmail.com>
2020-12-01 09:03:49 -0400

    Clean up 2020 day 1 solution

--- 2020/lib/day_01.ex
+++ 2020/lib/day_01.ex
@@ -4,24 +4,22 @@ defmodule Day01 do
   Find the two entries that sum to 2020 and multiply them together.
   """
   def part1(input) do
-    numbers =
-      input
-      |> Helpers.parse_to_integer_list("\n")
-
+    numbers = parse_numbers(input)
     result = for i <- numbers, j <- numbers, i + j == 2020, do: i * j
     hd(result)
   end
 
   @doc """
-  Part 1
-  Find the two entries that sum to 2020 and multiply them together.
+  Part 2
+  Find the three entries that sum to 2020 and multiply them together.
   """
   def part2(input) do
-    numbers =
-      input
-      |> Helpers.parse_to_integer_list("\n")
-
+    numbers = parse_numbers(input)
     result = for i <- numbers, j <- numbers, k <- numbers, i + j + k == 2020, do: i * j * k
     hd(result)
+  end
+
+  def parse_numbers(input) do
+    input |> Helpers.parse_to_integer_list("\n")
   end
 end
