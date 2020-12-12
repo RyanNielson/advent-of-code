aceb34d39f5829920fd0ec4133fe8f9ab90db276
Ryan Nielson <ryan.nielson@gmail.com>
2020-12-12 09:09:43 -0400

    Refactor day 12 solutions

--- 2020/lib/day_12.ex
+++ 2020/lib/day_12.ex
@@ -2,129 +2,56 @@ defmodule Day12 do
   def part1(input) do
     input
     |> parse()
-    |> navigate()
+    |> navigate({{0, 0}, {1, 0}}, &instruction/2)
     |> manhattan_distance()
   end
 
-  # TODO: Maybe flip ship and direction, and we might be able to share all this code.
   def part2(input) do
     input
     |> parse()
-    |> navigate_using_waypoint()
+    |> navigate({{0, 0}, {10, -1}}, &instruction_waypoint/2)
     |> manhattan_distance()
-
-    # |> manhattan_distance()
   end
 
-  defp manhattan_distance({x, y}) do
-    abs(x) + abs(y)
-  end
-
-  defp navigate(instructions) do
+  defp navigate(instructions, start, instruction_handler) do
     instructions
-    |> Enum.reduce({{0, 0}, {1, 0}}, fn instruction, acc ->
-      instruction(instruction, acc)
-    end)
+    |> Enum.reduce(start, &instruction_handler.(&1, &2))
     |> elem(0)
   end
 
-  defp instruction({"N", value}, {{x, y}, direction}) do
-    {{x, y - value}, direction}
-  end
+  defp instruction({"N", value}, {{x, y}, direction}), do: {{x, y - value}, direction}
+  defp instruction({"S", value}, {{x, y}, direction}), do: {{x, y + value}, direction}
+  defp instruction({"E", value}, {{x, y}, direction}), do: {{x + value, y}, direction}
+  defp instruction({"W", value}, {{x, y}, direction}), do: {{x - value, y}, direction}
 
-  defp instruction({"S", value}, {{x, y}, direction}) do
-    {{x, y + value}, direction}
-  end
+  defp instruction({"F", value}, {{x, y}, {dx, dy} = direction}),
+    do: {{x + dx * value, y + dy * value}, direction}
 
-  defp instruction({"E", value}, {{x, y}, direction}) do
-    {{x + value, y}, direction}
-  end
+  defp instruction({action, value}, {position, direction}),
+    do: {position, turn(action, value, direction)}
 
-  defp instruction({"W", value}, {{x, y}, direction}) do
-    {{x - value, y}, direction}
-  end
+  defp instruction_waypoint({"N", value}, {ship, {wx, wy}}), do: {ship, {wx, wy - value}}
+  defp instruction_waypoint({"S", value}, {ship, {wx, wy}}), do: {ship, {wx, wy + value}}
+  defp instruction_waypoint({"E", value}, {ship, {wx, wy}}), do: {ship, {wx + value, wy}}
+  defp instruction_waypoint({"W", value}, {ship, {wx, wy}}), do: {ship, {wx - value, wy}}
 
-  # TODO: Clean turn instructions up because they're basically the same.
-  defp instruction({"L", value}, {position, direction}) do
-    times = div(value, 90)
+  defp instruction_waypoint({"F", value}, {{x, y}, {wx, wy} = waypoint}),
+    do: {{x + wx * value, y + wy * value}, waypoint}
 
-    new_direction =
-      0..(times - 1)
-      |> Enum.reduce(direction, fn _, {dx, dy} ->
-        {dy, -dx}
-      end)
+  defp instruction_waypoint({action, value}, {position, waypoint}),
+    do: {position, turn(action, value, waypoint)}
 
-    {position, new_direction}
-  end
-
-  defp instruction({"R", value}, {position, direction}) do
-    times = div(value, 90)
-
-    new_direction =
-      0..(times - 1)
-      |> Enum.reduce(direction, fn _, {dx, dy} ->
-        {-dy, dx}
-      end)
-
-    {position, new_direction}
-  end
-
-  defp instruction({"F", value}, {{x, y}, {dx, dy} = direction}) do
-    {{x + dx * value, y + dy * value}, direction}
-  end
-
-  defp navigate_using_waypoint(instructions) do
-    instructions
-    |> Enum.reduce({{0, 0}, {10, -1}}, fn instruction, acc ->
-      instruction_waypoint(instruction, acc)
+  defp turn(action, value, direction) do
+    0..(div(value, 90) - 1)
+    |> Enum.reduce(direction, fn _, {wx, wy} ->
+      case action do
+        "L" -> {wy, -wx}
+        "R" -> {-wy, wx}
+      end
     end)
-    |> elem(0)
   end
 
-  defp instruction_waypoint({"N", value}, {ship, {wx, wy}}) do
-    {ship, {wx, wy - value}}
-  end
-
-  defp instruction_waypoint({"S", value}, {ship, {wx, wy}}) do
-    {ship, {wx, wy + value}}
-  end
-
-  defp instruction_waypoint({"E", value}, {ship, {wx, wy}}) do
-    {ship, {wx + value, wy}}
-  end
-
-  defp instruction_waypoint({"W", value}, {ship, {wx, wy}}) do
-    {ship, {wx - value, wy}}
-  end
-
-  # TODO: Clean turn instructions up because they're basically the same.
-  defp instruction_waypoint({"L", value}, {position, waypoint}) do
-    times = div(value, 90)
-
-    new_waypoint =
-      0..(times - 1)
-      |> Enum.reduce(waypoint, fn _, {wx, wy} ->
-        {wy, -wx}
-      end)
-
-    {position, new_waypoint}
-  end
-
-  defp instruction_waypoint({"R", value}, {position, waypoint}) do
-    times = div(value, 90)
-
-    new_waypoint =
-      0..(times - 1)
-      |> Enum.reduce(waypoint, fn _, {wx, wy} ->
-        {-wy, wx}
-      end)
-
-    {position, new_waypoint}
-  end
-
-  defp instruction_waypoint({"F", value}, {{x, y}, {wx, wy} = waypoint}) do
-    {{x + wx * value, y + wy * value}, waypoint}
-  end
+  defp manhattan_distance({x, y}), do: abs(x) + abs(y)
 
   defp parse(input) do
     input
