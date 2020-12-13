241c5ffebb513de82ef9109dde68dd0561c96253
Ryan Nielson <ryan.nielson@gmail.com>
2020-12-13 19:09:54 -0400

    Day 13 solutions

--- 2020/lib/day_13.ex
+++ 2020/lib/day_13.ex
@@ -15,6 +15,20 @@ defmodule Day13 do
     next_id * (next_time - earliest_departure)
   end
 
+  def part2(input) do
+    input
+    |> parse2()
+    |> Enum.reduce(fn {next_id, next_offset}, {jump, timestamp} ->
+      next =
+        timestamp
+        |> Stream.iterate(&(&1 + jump))
+        |> Enum.find(&(rem(&1 + next_offset, next_id) == 0))
+
+      {jump * next_id, next}
+    end)
+    |> elem(1)
+  end
+
   defp parse(input) do
     [earliest, id_string] =
       input
@@ -27,5 +41,17 @@ defmodule Day13 do
       |> Enum.map(&String.to_integer/1)
 
     {String.to_integer(earliest), ids}
+  end
+
+  defp parse2(input) do
+    [_, id_string] =
+      input
+      |> String.split("\n", trim: true)
+
+    id_string
+    |> String.split(",")
+    |> Enum.with_index()
+    |> Enum.reject(fn {id, _} -> id == "x" end)
+    |> Enum.map(fn {id, offset} -> {String.to_integer(id), offset} end)
   end
 end
--- 2020/test/day_13_test.exs
+++ 2020/test/day_13_test.exs
@@ -10,9 +10,14 @@ defmodule Day13Test do
       assert part1(input("day_13")) == 3269
     end
 
-    # test "part2" do
-    #   assert part2(input("day_12_example_1")) == 286
-    #   assert part2(input("day_12")) == 126_797
-    # end
+    test "part2" do
+      assert part2(input("day_13_example_1")) == 1_068_781
+      assert part2(input("day_13_example_2")) == 3417
+      assert part2(input("day_13_example_3")) == 754_018
+      assert part2(input("day_13_example_4")) == 779_210
+      assert part2(input("day_13_example_5")) == 1_261_476
+      assert part2(input("day_13_example_6")) == 1_202_161_486
+      assert part2(input("day_13")) == 672_754_131_923_874
+    end
   end
 end
