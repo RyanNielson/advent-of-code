c7d082f53b7da9b235169e04102f68c25450f88e
Ryan Nielson <ryan.nielson@gmail.com>
2020-12-18 08:44:20 -0400

    Day 18 part 2 solutions

--- 2020/lib/day_18.ex
+++ 2020/lib/day_18.ex
@@ -1,7 +1,9 @@
-# TODO: Can this just be in the module?
+# Overload operators to change precedence so that Code.eval_string
+# can do the heavy lifting for us.
 defmodule Overloads do
   def a <<< b, do: a + b
   def a >>> b, do: a * b
+  def a &&& b, do: a * b
 end
 
 defmodule Day18 do
@@ -10,6 +12,17 @@ defmodule Day18 do
   def part1(input) do
     input
     |> parse_1()
+    |> solve()
+  end
+
+  def part2(input) do
+    input
+    |> parse_2()
+    |> solve()
+  end
+
+  def solve(expressions) do
+    expressions
     |> Enum.map(&Code.eval_string(&1, [], __ENV__))
     |> Enum.map(&elem(&1, 0))
     |> Enum.sum()
@@ -19,6 +32,13 @@ defmodule Day18 do
     input
     |> String.replace("+", "<<<")
     |> String.replace("*", ">>>")
+    |> String.split("\n", trim: true)
+  end
+
+  defp parse_2(input) do
+    input
+    |> String.replace("+", "<<<")
+    |> String.replace("*", "&&&")
     |> String.split("\n", trim: true)
   end
 end
--- 2020/test/day_18_test.exs
+++ 2020/test/day_18_test.exs
@@ -12,11 +12,11 @@ defmodule Day18Test do
       assert part1(input("day_18")) == 1_451_467_526_514
     end
 
-    # test "part2" do
-    #   assert part2(input("day_18_example_1")) == 231
-    #   assert part2(input("day_18_example_2")) == 51
-    #   assert part2(input("day_18_example_3")) == 693_891
-    #   # assert part2(input("day_17")) == 1392693,891
-    # end
+    test "part2" do
+      assert part2(input("day_18_example_1")) == 231
+      assert part2(input("day_18_example_2")) == 51
+      assert part2(input("day_18_example_3")) == 693_891
+      assert part2(input("day_18")) == 224_973_686_321_527
+    end
   end
 end
