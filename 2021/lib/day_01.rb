aa1530913f55d389bfa37520cea29d1a479c2f54
Ryan Nielson <ryan.nielson@gmail.com>
2021-12-01 08:54:22 -0400

    2021 day 1 tweaks

--- 2021/lib/day_01.rb
+++ 2021/lib/day_01.rb
@@ -1,14 +1,12 @@
 class Day01
   def part1(input)
     measurements = input.split.map(&:to_i)
-    zipped = measurements[..-2].zip(measurements[1..])
-    zipped.select { |x, y| y > x }.count
+    measurements[..-2].zip(measurements[1..]).count { |x, y| y > x }
   end
 
   def part2(input)
     measurements = input.split.map(&:to_i)
     summed = measurements[..-3].zip(measurements[1..-2], measurements[2..]).map(&:sum)
-    zipped_summed = summed[..-2].zip(summed[1..])
-    zipped_summed.select { |x, y| y > x }.count
+    summed[..-2].zip(summed[1..]).count { |x, y| y > x }
   end
 end
