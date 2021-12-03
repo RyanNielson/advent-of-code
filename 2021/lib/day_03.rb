0527cfb567d02dfe596318748e7ae2c6be924c83
Ryan Nielson <ryan.nielson@gmail.com>
2021-12-03 10:34:36 -0400

    2021 Day 3 some small refactoring

--- 2021/lib/day_03.rb
+++ 2021/lib/day_03.rb
@@ -9,42 +9,24 @@ class Day03
   end
 
   def part2(input)
-    values = input
-             .split("\n")
-             .map(&:chars)
-
-    oxygen_generator_rating = oxygen_generator_rating(values)
-    co2_scrubber_rating = co2_scrubber_rating(values)
-
-    oxygen_generator_rating * co2_scrubber_rating
+    input
+      .split("\n")
+      .map(&:chars)
+      .then { life_support_rating(_1) }
   end
 
   private
 
-  def oxygen_generator_rating(values)
-    value_length = values
-                   .map(&:length)
-                   .max
-
-    value_length.times do |i|
-      bits = values.transpose[i]
-      most_common = most_common(bits)
-      values = values.filter { |num| num[i] == most_common }
-      break if values.count == 1
-    end
-
-    values.flatten.join.to_i(2)
+  def life_support_rating(values)
+    num_bits = values.map(&:length).max
+    calculate_rating(values, num_bits, :most_common) * calculate_rating(values, num_bits, :least_common)
   end
 
-  def co2_scrubber_rating(values)
-    value_length = values
-                   .map(&:length)
-                   .max
-
-    value_length.times do |i|
+  def calculate_rating(values, num_bits, type)
+    num_bits.times do |i|
       bits = values.transpose[i]
-      most_common = least_common(bits)
-      values = values.filter { |num| num[i] == most_common }
+      bit = method(type).call(bits)
+      values = values.filter { |num| num[i] == bit }
       break if values.count == 1
     end
 
@@ -52,8 +34,7 @@ class Day03
   end
 
   def most_common(bits)
-    counts = bits
-             .tally
+    counts = bits.tally
 
     return "1" if counts["0"] == counts["1"]
 
@@ -63,8 +44,7 @@ class Day03
   end
 
   def least_common(bits)
-    counts = bits
-             .tally
+    counts = bits.tally
 
     return "0" if counts["0"] == counts["1"]
 
