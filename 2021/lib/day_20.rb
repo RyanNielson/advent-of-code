61d7d31c65fe7c7c8fb4757951f673491fd7cbe2
Ryan Nielson <ryan.nielson@gmail.com>
2021-12-20 10:05:53 -0400

    Refactor 2021 Day 20

--- 2021/lib/day_20.rb
+++ 2021/lib/day_20.rb
@@ -1,62 +1,77 @@
-require "active_support/all"
-
 class Day20
   def part1(input)
-    algorithm, image = parse(input)
-    enhance_and_count(image, algorithm, 2)
+    enhance_and_count(Image.new(input), 2)
   end
 
   def part2(input)
-    algorithm, image = parse(input)
-    enhance_and_count(image, algorithm, 50)
+    enhance_and_count(Image.new(input), 50)
   end
 
-  def enhance_and_count(image, algorithm, steps)
-    steps.times.reduce(image) do |new_image, i|
-      if algorithm.first == "#"
-        new_image.default = i.odd? ? "#" : "."
-      end
-
-      enhance(algorithm, new_image)
-    end.values.count("#")
-  end
-
-  def enhance(algorithm, image)
-    new_image = {}
-    min_x, max_x = image.keys.minmax_by(&:x)
-    min_y, max_y = image.keys.minmax_by(&:y)
-
-    ((min_y.y - 1)..(max_y.y + 1)).each do |y|
-      ((min_x.x - 1)..(max_x.x + 1)).each do |x|
-        position = Position.new(x, y)
-        number = [Position.new(x - 1, y - 1),
-                  Position.new(x, y - 1),
-                  Position.new(x + 1, y - 1),
-                  Position.new(x - 1, y),
-                  position,
-                  Position.new(x + 1, y),
-                  Position.new(x - 1, y + 1),
-                  Position.new(x, y + 1),
-                  Position.new(x + 1, y + 1)].map do |pos|
-          image[pos] == "#" ? "1" : "0"
-        end.join.to_i(2)
-
-        new_image[position] = algorithm[number]
-      end
-    end
-
-    new_image
-  end
-
-  def parse(input)
-    algorithm_input, image_input = input.split("\n\n")
-
-    image = image_input.split("\n").each_with_index.each_with_object(Hash.new(".")) do |(row, y), acc|
-      row.chars.each_with_index { |pixel, x| acc[Position.new(x, y)] = pixel }
-    end
-
-    [algorithm_input.chars, image]
+  def enhance_and_count(image, steps)
+    steps.times { image.enhance }
+    image.count
   end
 end
 
 Position = Struct.new(:x, :y)
+
+class Image
+  def initialize(input)
+    @algorithm, @pixels = parse(input)
+    @enhancements = 0
+  end
+
+  def enhance
+    if @algorithm.first == "#"
+      @pixels.default = @enhancements.odd? ? "#" : "."
+    end
+
+    new_pixels = {}
+    min_x, max_x, min_y, max_y = expanded_bounds
+
+    (min_y..max_y).each do |y|
+      (min_x..max_x).each do |x|
+        position = Position.new(x, y)
+        new_pixels[position] = @algorithm[determine_number(position)]
+      end
+    end
+
+    @enhancements += 1
+    @pixels = new_pixels
+  end
+
+  def determine_number(position)
+    x, y = position.values
+    [Position.new(x - 1, y - 1),
+     Position.new(x, y - 1),
+     Position.new(x + 1, y - 1),
+     Position.new(x - 1, y),
+     position,
+     Position.new(x + 1, y),
+     Position.new(x - 1, y + 1),
+     Position.new(x, y + 1),
+     Position.new(x + 1, y + 1)].map do |check_position|
+      @pixels[check_position] == "#" ? "1" : "0"
+    end.join.to_i(2)
+  end
+
+  def parse(input)
+    algorithm_input, pixels_input = input.split("\n\n")
+
+    pixels = pixels_input.split("\n").each_with_index.each_with_object(Hash.new(".")) do |(row, y), acc|
+      row.chars.each_with_index { |pixel, x| acc[Position.new(x, y)] = pixel }
+    end
+
+    [algorithm_input.chars, pixels]
+  end
+
+  def count
+    @pixels.values.count("#")
+  end
+
+  def expanded_bounds
+    min_x, max_x = @pixels.keys.minmax_by(&:x)
+    min_y, max_y = @pixels.keys.minmax_by(&:y)
+    [min_x.x - 1, max_x.x + 1, min_y.y - 1, max_y.y + 1]
+  end
+end
