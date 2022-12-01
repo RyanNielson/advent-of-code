4c369784e882b1687680c8ba9034bd71f16a5379
Ryan Nielson <ryan.nielson@gmail.com>
2022-12-01 11:34:35 +0000

    2022 Day 1

--- 2022/lib/day_01.ex
+++ 2022/lib/day_01.ex
@@ -1,9 +1,26 @@
 defmodule Day01 do
   def part1(input) do
     input
+    |> elf_calories()
+    |> Enum.max()
   end
 
   def part2(input) do
     input
+    |> elf_calories()
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  defp elf_calories(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(fn elf ->
+      elf
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
   end
 end
--- 2022/test/day_01_test.exs
+++ 2022/test/day_01_test.exs
@@ -5,12 +5,12 @@ defmodule Day01Test do
   import Day01
 
   test "part1" do
-    assert part1(input("day_01_example_1")) == 123
-    assert part1(input("day_01")) == 456
+    assert part1(input("day_01_example_1")) == 24000
+    assert part1(input("day_01")) == 69177
   end
 
   test "part2" do
-    assert part2(input("day_01_example_1")) == 123
-    assert part2(input("day_01")) == 456
+    assert part2(input("day_01_example_1")) == 45000
+    assert part2(input("day_01")) == 207_456
   end
 end
