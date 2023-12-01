bede8a6b3a030aa9be95f045126048d9d0845630
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-01 13:59:58 -0400

    day 1 refactor to share code between part 1 and 2

--- 2023/src/bin/day_01.rs
+++ 2023/src/bin/day_01.rs
@@ -1,49 +1,47 @@
 fn part_1(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let first_digit = line.chars().find(|&c| c.is_ascii_digit()).unwrap();
-            let last_digit = line.chars().rev().find(|&c| c.is_ascii_digit()).unwrap();
-            let num_string = format!("{first_digit}{last_digit}");
-            num_string.parse::<u32>().unwrap()
-        })
-        .sum::<u32>()
+    sum_calibration_values(input, false)
 }
 
 fn part_2(input: &str) -> u32 {
-    let valid_numbers = [
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-    ];
+    sum_calibration_values(input, true)
+}
 
+fn sum_calibration_values(input: &str, use_spelled: bool) -> u32 {
     input
         .lines()
-        .map(|line| {
-            let mut matches = (0..line.len()).filter_map(|i| {
-                let slice = &line[i..];
-
-                for (spelled, digit) in valid_numbers {
-                    if slice.starts_with(digit) || slice.starts_with(spelled) {
-                        return Some(digit);
-                    }
-                }
-
-                None
-            });
-
-            let first_digit = matches.next().unwrap();
-            let last_digit = matches.last().unwrap_or(first_digit);
-            let num_string = format!("{first_digit}{last_digit}");
-            num_string.parse::<u32>().unwrap()
-        })
+        .map(|line| calibration_value(line, use_spelled))
         .sum::<u32>()
+}
+
+fn calibration_value(line: &str, use_spelled: bool) -> u32 {
+    let valid_numbers = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+
+    let mut matches = (0..line.len()).filter_map(|i| {
+        let slice = &line[i..];
+
+        for (spelled, digit) in valid_numbers {
+            if slice.starts_with(&digit.to_string()) || (use_spelled && slice.starts_with(spelled))
+            {
+                return Some(digit);
+            }
+        }
+
+        None
+    });
+
+    let first_digit = matches.next().unwrap();
+    let last_digit = matches.last().unwrap_or(first_digit);
+    first_digit * 10 + last_digit
 }
 
 fn main() {
