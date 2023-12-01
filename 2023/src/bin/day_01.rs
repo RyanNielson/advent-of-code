15d4928bf829db4239b5495ae1403708c30503f4
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-01 11:22:12 -0400

    day 1 part 2

--- 2023/src/bin/day_01.rs
+++ 2023/src/bin/day_01.rs
@@ -10,9 +10,40 @@ fn part_1(input: &str) -> u32 {
         .sum::<u32>()
 }
 
-fn part_2(input: &str) -> &str {
-    println!("{input}");
+fn part_2(input: &str) -> u32 {
+    let valid_numbers = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
     input
+        .lines()
+        .map(|line| {
+            let mut matches = (0..line.len()).filter_map(|i| {
+                let slice = &line[i..];
+
+                for (spelled, digit) in valid_numbers {
+                    if slice.starts_with(digit) || slice.starts_with(spelled) {
+                        return Some(digit);
+                    }
+                }
+
+                None
+            });
+
+            let first_digit = matches.next().unwrap();
+            let last_digit = matches.last().unwrap_or(first_digit);
+            let num_string = format!("{first_digit}{last_digit}");
+            num_string.parse::<u32>().unwrap()
+        })
+        .sum::<u32>()
 }
 
 fn main() {
@@ -33,6 +64,7 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(include_str!("inputs/day_01")), "input");
+        assert_eq!(part_2(include_str!("inputs/day_01_example_2")), 281);
+        assert_eq!(part_2(include_str!("inputs/day_01")), 54_578);
     }
 }
