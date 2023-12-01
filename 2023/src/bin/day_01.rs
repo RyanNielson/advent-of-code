4c69ef889323f632ed18e71cbb2b70939e987d6d
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-01 08:35:44 -0400

    day 1 part 1

--- 2023/src/bin/day_01.rs
+++ 2023/src/bin/day_01.rs
@@ -1,6 +1,13 @@
-fn part_1(input: &str) -> &str {
-    println!("{input}");
+fn part_1(input: &str) -> u32 {
     input
+        .lines()
+        .map(|line| {
+            let first_digit = line.chars().find(|&c| c.is_ascii_digit()).unwrap();
+            let last_digit = line.chars().rev().find(|&c| c.is_ascii_digit()).unwrap();
+            let num_string = format!("{first_digit}{last_digit}");
+            num_string.parse::<u32>().unwrap()
+        })
+        .sum::<u32>()
 }
 
 fn part_2(input: &str) -> &str {
@@ -20,7 +27,8 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(include_str!("inputs/day_01")), "input");
+        assert_eq!(part_1(include_str!("inputs/day_01_example_1")), 142);
+        assert_eq!(part_1(include_str!("inputs/day_01")), 55_208);
     }
 
     #[test]
