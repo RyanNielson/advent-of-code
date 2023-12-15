259f79715d3f1e84507500d07d0e49de10a4863a
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-15 11:43:22 -0400

    day 15 part 2

--- 2023/src/bin/day_15.rs
+++ 2023/src/bin/day_15.rs
@@ -1,18 +1,76 @@
-use std::ops::Rem;
+use std::{collections::HashMap, ops::Rem, str::FromStr};
 
 fn part_1(input: &str) -> usize {
-    input
+    input.split(',').map(hash).sum()
+}
+
+fn part_2(input: &str) -> usize {
+    let mut boxes = (0..256)
+        .map(|i| (i, Vec::<Lens>::new()))
+        .collect::<HashMap<_, _>>();
+
+    let lenses = input
         .split(',')
-        .map(|step| {
-            step.as_bytes()
+        .map(|step| Lens::from_str(step).unwrap())
+        .collect::<Vec<_>>();
+
+    for lens in lenses {
+        let vec = boxes.get_mut(&hash(&lens.label)).unwrap();
+
+        match lens.focal_length {
+            Some(_) => {
+                if let Some(index) = vec.iter().position(|l| l.label == lens.label) {
+                    vec[index] = lens;
+                } else {
+                    vec.push(lens);
+                }
+            }
+            None => {
+                if let Some(index) = vec.iter().position(|l| l.label == lens.label) {
+                    vec.remove(index);
+                }
+            }
+        }
+    }
+
+    boxes
+        .iter()
+        .flat_map(|(id, lenses)| {
+            lenses
                 .iter()
-                .fold(0, |acc, c| ((acc + *c as usize) * 17).rem(256))
+                .enumerate()
+                .map(move |(i, lens)| (id + 1) * (i + 1) * lens.focal_length.unwrap())
         })
         .sum()
 }
 
-fn part_2(_input: &str) -> usize {
-    1
+#[derive(Debug)]
+struct Lens {
+    label: String,
+    focal_length: Option<usize>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseLensError;
+
+impl FromStr for Lens {
+    type Err = ParseLensError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut pieces = s.split(['=', '-']);
+        let label = pieces.next().unwrap().to_string();
+        let focal_length = pieces.next().and_then(|f| f.parse().ok());
+        Ok(Self {
+            label,
+            focal_length,
+        })
+    }
+}
+
+fn hash(step: &str) -> usize {
+    step.as_bytes()
+        .iter()
+        .fold(0, |acc, c| ((acc + *c as usize) * 17).rem(256))
 }
 
 fn main() {
@@ -33,7 +91,7 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        // assert_eq!(part_2(include_str!("inputs/day_15_example_1")), 5905);
-        // assert_eq!(part_2(include_str!("inputs/day_15")), 249666369);
+        assert_eq!(part_2(include_str!("inputs/day_15_example_1")), 145);
+        assert_eq!(part_2(include_str!("inputs/day_15")), 239484);
     }
 }
