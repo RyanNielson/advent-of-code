4ac9475489b709e8fdbaeb162d6a7ac9242086b6
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-18 08:58:14 -0400

    day 18 part 2

--- 2023/src/bin/day_18.rs
+++ 2023/src/bin/day_18.rs
@@ -45,13 +45,63 @@ fn part_1(input: &str) -> isize {
     edge_count + inner
 }
 
-fn part_2(input: &str) -> usize {
-    2
+fn part_2(input: &str) -> isize {
+    let plan = input
+        .lines()
+        .map(|line| {
+            let mut pieces = line.split_whitespace();
+            let _direction = pieces.next().unwrap().chars().next().unwrap();
+            let _amount = pieces.next().unwrap().parse::<isize>().unwrap();
+            let (colour_amount, colour_direction) = pieces
+                .next()
+                .unwrap()
+                .trim_matches(|c| c == '(' || c == ')' || c == '#')
+                .split_at(5);
+
+            let direction = match colour_direction {
+                "0" => 'R',
+                "1" => 'D',
+                "2" => 'L',
+                "3" => 'U',
+                _ => unreachable!(),
+            };
+
+            let amount = isize::from_str_radix(colour_amount, 16).unwrap();
+
+            // println!("{:?}", colour);
+            (direction, amount)
+        })
+        .collect_vec();
+
+    let mut current_vertex = Position { x: 0, y: 0 };
+    let mut vertices = vec![current_vertex];
+    let mut edge_count = 0;
+
+    for (direction, amount) in plan {
+        match direction {
+            'R' => current_vertex.x += amount,
+            'L' => current_vertex.x -= amount,
+            'U' => current_vertex.y -= amount,
+            'D' => current_vertex.y += amount,
+            _ => unreachable!(),
+        };
+
+        edge_count += amount;
+
+        vertices.push(current_vertex);
+    }
+
+    // Use shoelace formula + pick's algorithm
+    let mut area = 0;
+    for (a, b) in vertices.iter().tuple_windows() {
+        area += a.x * b.y - b.x * a.y;
+    }
+    area /= 2;
+
+    let inner = area + 1 - edge_count / 2;
+
+    edge_count + inner
 }
-
-// fn parse(input: &str) -> Vec<(char, usize, St) {
-
-// }
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 struct Position {
@@ -77,7 +127,10 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        // assert_eq!(part_2(include_str!("inputs/day_16_example_1")), 51);
-        // assert_eq!(part_2(include_str!("inputs/day_16")), 8437);
+        assert_eq!(
+            part_2(include_str!("inputs/day_18_example_1")),
+            952408144115
+        );
+        assert_eq!(part_2(include_str!("inputs/day_18")), 8437);
     }
 }
