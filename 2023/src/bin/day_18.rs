690d6bd681c01218f25ff86acda358973a74a312
Ryan Nielson <ryan.nielson@gmail.com>
2023-12-18 09:03:07 -0400

    day 18 refactor

--- 2023/src/bin/day_18.rs
+++ 2023/src/bin/day_18.rs
@@ -1,75 +1,42 @@
 use itertools::Itertools;
 
 fn part_1(input: &str) -> isize {
-    let plan = input
-        .lines()
-        .map(|line| {
-            let mut pieces = line.split_whitespace();
-            let direction = pieces.next().unwrap().chars().next().unwrap();
-            let amount = pieces.next().unwrap().parse::<isize>().unwrap();
-            let colour = pieces
-                .next()
-                .unwrap()
-                .trim_matches(|c| c == '(' || c == ')');
-            (direction, amount, colour)
-        })
-        .collect_vec();
-
-    let mut current_vertex = Position { x: 0, y: 0 };
-    let mut vertices = vec![current_vertex];
-    let mut edge_count = 0;
-
-    for (direction, amount, colour) in plan {
-        match direction {
-            'R' => current_vertex.x += amount,
-            'L' => current_vertex.x -= amount,
-            'U' => current_vertex.y -= amount,
-            'D' => current_vertex.y += amount,
-            _ => unreachable!(),
-        };
-
-        edge_count += amount;
-
-        vertices.push(current_vertex);
-    }
-
-    // Use shoelace formula + pick's algorithm
-    let mut area = 0;
-    for (a, b) in vertices.iter().tuple_windows() {
-        area += a.x * b.y - b.x * a.y;
-    }
-    area /= 2;
-
-    let inner = area + 1 - edge_count / 2;
-
-    edge_count + inner
+    solve(input, false)
 }
 
 fn part_2(input: &str) -> isize {
+    solve(input, true)
+}
+
+fn solve(input: &str, use_color: bool) -> isize {
     let plan = input
         .lines()
         .map(|line| {
             let mut pieces = line.split_whitespace();
-            let _direction = pieces.next().unwrap().chars().next().unwrap();
-            let _amount = pieces.next().unwrap().parse::<isize>().unwrap();
-            let (colour_amount, colour_direction) = pieces
-                .next()
-                .unwrap()
-                .trim_matches(|c| c == '(' || c == ')' || c == '#')
-                .split_at(5);
 
-            let direction = match colour_direction {
-                "0" => 'R',
-                "1" => 'D',
-                "2" => 'L',
-                "3" => 'U',
-                _ => unreachable!(),
-            };
+            if use_color {
+                let (colour_amount, colour_direction) = pieces
+                    .nth(2)
+                    .unwrap()
+                    .trim_matches(|c| c == '(' || c == ')' || c == '#')
+                    .split_at(5);
 
-            let amount = isize::from_str_radix(colour_amount, 16).unwrap();
+                let direction = match colour_direction {
+                    "0" => 'R',
+                    "1" => 'D',
+                    "2" => 'L',
+                    "3" => 'U',
+                    _ => unreachable!(),
+                };
 
-            // println!("{:?}", colour);
-            (direction, amount)
+                let amount = isize::from_str_radix(colour_amount, 16).unwrap();
+
+                (direction, amount)
+            } else {
+                let direction = pieces.next().unwrap().chars().next().unwrap();
+                let amount = pieces.next().unwrap().parse::<isize>().unwrap();
+                (direction, amount)
+            }
         })
         .collect_vec();
 
@@ -131,6 +98,6 @@ mod tests {
             part_2(include_str!("inputs/day_18_example_1")),
             952408144115
         );
-        assert_eq!(part_2(include_str!("inputs/day_18")), 8437);
+        assert_eq!(part_2(include_str!("inputs/day_18")), 57196493937398);
     }
 }
