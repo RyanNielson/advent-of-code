4bc9c81842547a6d3822ed250b782a9a2f43f632
Ryan Nielson <ryan.nielson@gmail.com>
2024-11-30 20:03:12 -0400

    2024 set-up

---
+++ 2024/day01/day01_test.go
@@ -0,0 +1,19 @@
+package day01
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	result := Part1()
+
+	if result != 0 {
+		t.Errorf("expected '%d' but got '%d'", 0, result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part1()
+
+	if result != 0 {
+		t.Errorf("expected '%d' but got '%d'", 0, result)
+	}
+}
