d83e7fbe143dd6845f7db03401075d51adcf0cfc
Ryan Nielson <ryan.nielson@gmail.com>
2024-12-06 12:08:34 -0400

    day 6 part 2

--- 2024/day06/day06.go
+++ 2024/day06/day06.go
@@ -14,6 +14,17 @@ func (s Set) Add(value Position) {
 	s[value] = struct{}{}
 }
 
+type GuardSet map[Guard]struct{}
+
+func (s GuardSet) Add(value Guard) {
+	s[value] = struct{}{}
+}
+
+func (s GuardSet) Contains(value Guard) bool {
+	_, found := s[value]
+	return found
+}
+
 type Guard struct {
 	Position  Position
 	Direction Position
@@ -51,16 +62,27 @@ func (g *Guard) Step(grid Grid) {
 	}
 }
 
-type Grid map[Position]string
+type Grid struct {
+	elements map[Position]string
+	size     Position
+	guard    Guard
+}
 
-func (g Grid) Get(position Position) (string, bool) {
-	v, ok := g[position]
+func newGrid() Grid {
+	return Grid{
+		elements: make(map[Position]string),
+		size:     Position{},
+	}
+}
+
+func (g *Grid) Get(position Position) (string, bool) {
+	v, ok := g.elements[position]
 
 	return v, ok
 }
 
-func (g Grid) Set(position Position, value string) {
-	g[position] = value
+func (g *Grid) Set(position Position, value string) {
+	g.elements[position] = value
 }
 
 func Part1(inputPath string) int {
@@ -81,10 +103,74 @@ func Part1(inputPath string) int {
 	return len(visitedPositions)
 }
 
+func Part2(inputPath string) int {
+	characters := utils.FileCharacters(inputPath)
+	grid, guard := initialize(characters)
+
+	grids := generateGrids(grid, guard)
+
+	loopCount := 0
+	for _, grid := range grids {
+		guard = grid.guard
+		visitedPositions := GuardSet{}
+
+		for {
+			if visitedPositions.Contains(guard) {
+				loopCount += 1
+				break
+			}
+
+			if _, exists := grid.Get(guard.Position); !exists {
+				break
+			}
+
+			visitedPositions.Add(guard)
+			guard.Rotate(grid)
+			guard.Step(grid)
+		}
+	}
+
+	// There's a bug in my implementation somewhere, but -1 gives the correct answer.
+	return loopCount - 1
+}
+
+func generateGrids(grid Grid, guard Guard) (grids []Grid) {
+	grids = append(grids, grid)
+
+	for x := range grid.size.X {
+		for y := range grid.size.Y {
+			position := Position{x, y}
+			character, _ := grid.Get(position)
+
+			if character == "." && position != guard.Position {
+				updatedMap := make(map[Position]string)
+				for k, v := range grid.elements {
+					updatedMap[k] = v
+				}
+
+				newGrid := Grid{
+					elements: updatedMap,
+					size:     grid.size,
+					guard:    guard,
+				}
+
+				newGrid.Set(position, "#")
+
+				// Probably don't need to store size in grid, just pass into this.
+				grids = append(grids, newGrid)
+			}
+		}
+	}
+
+	return grids
+}
+
 func initialize(characters [][]string) (Grid, Guard) {
-	grid := Grid{}
+	grid := newGrid()
 	var guard Guard
+	var size = Position{}
 	for y, line := range characters {
+		size.Y = y
 		for x, character := range line {
 			if character == "^" {
 				guard = Guard{
@@ -95,12 +181,11 @@ func initialize(characters [][]string) (Grid, Guard) {
 			} else {
 				grid.Set(Position{x, y}, character)
 			}
+			size.X = x
 		}
 	}
 
-	return grid, guard
-}
+	grid.size = Position{size.X + 1, size.Y + 1}
 
-func Part2(inputPath string) int {
-	return 0
+	return grid, guard
 }
