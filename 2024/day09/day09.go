02e441fd7fcf4cd3f90aea0c5db4d8bcc945ad9a
Ryan Nielson <ryan.nielson@gmail.com>
2024-12-09 14:31:55 -0400

    day 9 part 2 gross

--- 2024/day09/day09.go
+++ 2024/day09/day09.go
@@ -47,7 +47,7 @@ func Part1(inputPath string) int {
 		if firstEmptyIndex > lastFileBlockIndex {
 			break
 		}
-
+		// slices.Replace(string, start, end, []string)
 		firstValue := blocks[firstEmptyIndex]
 		lastValue := blocks[lastFileBlockIndex]
 
@@ -65,5 +65,88 @@ func Part1(inputPath string) int {
 }
 
 func Part2(inputPath string) int {
-	return 0
+	characters := utils.FileCharacters(inputPath)[0]
+	var numbers []int
+	for _, character := range characters {
+		num, _ := strconv.Atoi(character)
+		numbers = append(numbers, num)
+	}
+
+	var blocks []string
+	id := 0
+	isFile := true
+	highestFileNumber := 0
+	for _, number := range numbers {
+		if isFile {
+			idString := strconv.Itoa(id)
+			for i := 0; i < number; i++ {
+				blocks = append(blocks, idString)
+			}
+			highestFileNumber = id
+			id += 1
+		} else {
+			for i := 0; i < number; i++ {
+				blocks = append(blocks, ".")
+			}
+		}
+
+		isFile = !isFile
+	}
+
+	for highest := highestFileNumber; highest >= 0; highest-- {
+		highestFileNumberString := strconv.Itoa(highest)
+		fileStart := slices.Index(blocks, highestFileNumberString)
+		fileEnd := fileStart
+		for i := fileStart; i < len(blocks); i++ {
+			if blocks[i] == highestFileNumberString {
+				fileEnd = i
+			} else {
+				break
+			}
+		}
+
+		fileBlocksSlice := blocks[fileStart : fileEnd+1]
+		fileBlocksSize := len(fileBlocksSlice)
+
+		count := 0
+		emptyEndIndex := -1
+		for i, block := range blocks {
+			if block == "." {
+				count += 1
+			} else {
+				count = 0
+			}
+
+			if fileBlocksSize == count {
+				emptyEndIndex = i
+				break
+			}
+		}
+
+		if emptyEndIndex == -1 {
+			continue
+		}
+
+		emptyStart := emptyEndIndex - fileBlocksSize + 1
+
+		if emptyStart > fileStart {
+			continue
+		}
+
+		for i := 0; i < fileBlocksSize; i++ {
+			emptyValue := blocks[emptyStart+i]
+			fileValue := blocks[fileStart+i]
+
+			blocks[emptyStart+i] = fileValue
+			blocks[fileStart+i] = emptyValue
+		}
+	}
+
+	checksum := 0
+	for i, block := range blocks {
+		blockInt, _ := strconv.Atoi(block)
+		checksum += i * blockInt
+	}
+
+	return checksum
 }
--- 2024/day09/day09_test.go
+++ 2024/day09/day09_test.go
@@ -11,7 +11,7 @@ func TestPart1(t *testing.T) {
 	assert.Equal(t, 6390180901651, Part1("input"))
 }
 
-// func TestPart2(t *testing.T) {
-// 	assert.Equal(t, 34, Part2("input_example_1"))
-// 	assert.Equal(t, 1032, Part2("input"))
-// }
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 2858, Part2("input_example_1"))
+	assert.Equal(t, 6412390114238, Part2("input"))
+}
