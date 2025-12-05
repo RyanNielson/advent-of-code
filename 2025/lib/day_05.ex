502f5d65b66cdc6afabd6f78f93d37dec1bd2ca8
Ryan Nielson <ryan.nielson@gmail.com>
2025-12-05 08:28:34 -0400

    Day 5 part 2 and refactor

--- 2025/lib/day_05.ex
+++ 2025/lib/day_05.ex
@@ -1,6 +1,25 @@
 defmodule Day05 do
-  @spec part_1(any()) :: none()
   def part_1(input) do
+    {ranges, available} =
+      input
+      |> parse()
+
+    available
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.count(fn ingredient ->
+      Enum.any?(ranges, fn range -> ingredient in range end)
+    end)
+  end
+
+  def part_2(input) do
+    input
+    |> parse()
+    |> elem(0)
+    |> Enum.map(&Range.size/1)
+    |> Enum.sum()
+  end
+
+  defp parse(input) do
     {ranges, available} =
       input
       |> String.split("\n\n")
@@ -13,15 +32,25 @@ defmodule Day05 do
         [start, finish] = range |> String.split("-") |> Enum.map(&String.to_integer/1)
         start..finish
       end)
+      |> simplify_ranges()
 
-    available
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.count(fn ingredient ->
-      Enum.any?(ranges, fn range -> ingredient in range end)
-    end)
+    {ranges, available}
   end
 
-  def part_2(input) do
-    input
+  defp simplify_ranges(ranges) do
+    ranges
+    |> Enum.sort_by(& &1.first)
+    |> Enum.reduce([], fn range, acc ->
+      case acc do
+        [] ->
+          [range]
+
+        [prev | rest] ->
+          # If ranges overlap or touch merge them
+          if range.first <= prev.last + 1,
+            do: [prev.first..max(prev.last, range.last) | rest],
+            else: [range | acc]
+      end
+    end)
   end
 end
--- 2025/test/day_05_test.exs
+++ 2025/test/day_05_test.exs
@@ -8,8 +8,8 @@ defmodule Day05Test do
     assert Day05.part_1(input("day_05")) == 735
   end
 
-  # test "part2" do
-  #   assert Day04.part_2(input("day_04_example_1")) == 43
-  #   assert Day04.part_2(input("day_04")) == 9784
-  # end
+  test "part2" do
+    assert Day05.part_2(input("day_05_example_1")) == 14
+    assert Day05.part_2(input("day_05")) == 344_306_344_403_172
+  end
 end
