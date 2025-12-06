a24edb476912d941637db09f67ca1b07e4a50a3a
Ryan Nielson <ryan.nielson@gmail.com>
2025-12-06 12:35:01 -0400

    Day 6 part 2

--- 2025/lib/day_06.ex
+++ 2025/lib/day_06.ex
@@ -25,5 +25,47 @@ defmodule Day06 do
 
   def part_2(input) do
     input
+    |> String.split("\n", trim: false)
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.chunk_by(fn col ->
+      Enum.any?(col, &(&1 in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]))
+    end)
+    |> Enum.map(fn group ->
+      group
+      |> Enum.map_reduce(nil, fn operand, operator ->
+        operator =
+          cond do
+            "+" in operand -> "+"
+            "-" in operand -> "-"
+            "*" in operand -> "*"
+            "/" in operand -> "/"
+            true -> operator
+          end
+
+        operand =
+          operand
+          |> Enum.join("")
+          |> String.replace([" ", "+", "-", "*", "/"], "")
+          |> String.trim()
+
+        {operand, operator}
+      end)
+    end)
+    |> Enum.reject(fn {_operands, operator} -> is_nil(operator) end)
+    |> Enum.map(fn {operands, operator} ->
+      operands
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reduce(fn operand, acc ->
+        case operator do
+          "+" -> operand + acc
+          "-" -> operand - acc
+          "*" -> operand * acc
+          "/" -> operand / acc
+        end
+      end)
+    end)
+    |> Enum.sum()
   end
 end
--- 2025/test/day_06_test.exs
+++ 2025/test/day_06_test.exs
@@ -4,12 +4,12 @@ defmodule Day06Test do
   import TestHelper
 
   test "part1" do
-    assert Day06.part_1(input("day_06_example_1")) == 4_277_556
-    assert Day06.part_1(input("day_06")) == 6_635_273_135_233
+    assert Day06.part_1(input("day_06_example_1", false)) == 4_277_556
+    assert Day06.part_1(input("day_06", false)) == 6_635_273_135_233
   end
 
-  # test "part2" do
-  #   assert Day05.part_2(input("day_05_example_1")) == 14
-  #   assert Day05.part_2(input("day_05")) == 344_306_344_403_172
-  # end
+  test "part2" do
+    assert Day06.part_2(input("day_06_example_1", false)) == 3_263_827
+    assert Day06.part_2(input("day_06", false)) == 12_542_543_681_221
+  end
 end
