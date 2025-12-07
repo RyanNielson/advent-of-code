ef7c336c7acab2fa57fe466cc082a889286ffbf6
Ryan Nielson <ryan.nielson@gmail.com>
2025-12-07 10:07:57 -0400

    Day 7 refactor

--- 2025/lib/day_07.ex
+++ 2025/lib/day_07.ex
@@ -1,74 +1,49 @@
 defmodule Day07 do
   def part_1(input) do
-    rows =
-      input
-      |> String.split()
-      |> Enum.map(&String.graphemes/1)
-
-    start = rows |> hd() |> Enum.find_index(fn x -> x == "S" end)
-    starting_beams = MapSet.new([start])
-
-    rows
-    |> tl()
-    |> Enum.reduce({starting_beams, 0}, fn row, {beams, split_count} ->
-      if "^" not in row do
-        {beams, split_count}
-      else
-        beams
-        |> Enum.reduce({MapSet.new(), split_count}, fn beam, {next_beams, new_split_count} ->
-          case row |> Enum.at(beam) do
-            "." ->
-              {next_beams |> MapSet.put(beam), new_split_count}
-
-            "^" ->
-              {next_beams |> MapSet.put(beam - 1) |> MapSet.put(beam + 1), new_split_count + 1}
-          end
-        end)
-      end
-    end)
+    input
+    |> solve()
     |> elem(1)
   end
 
   def part_2(input) do
+    input
+    |> solve()
+    |> elem(0)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  defp solve(input) do
     rows =
       input
       |> String.split()
       |> Enum.map(&String.graphemes/1)
 
     start = rows |> hd() |> Enum.find_index(fn x -> x == "S" end)
-    starting_beams = Map.new([{start, 1}])
 
     rows
     |> tl()
-    |> Enum.reduce({starting_beams, 0}, fn row, {beams, split_count} ->
-      {row, beams, split_count} |> dbg()
-
+    |> Enum.reduce({Map.new([{start, 1}]), 0}, fn row, {beams, split_count} ->
       if "^" not in row do
         {beams, split_count}
       else
         beams
         |> Enum.reduce({Map.new(), split_count}, fn {beam, timelines},
                                                     {next_beams, new_split_count} ->
-          {beam, timelines, row} |> dbg()
-
           case Enum.at(row, beam) do
             "." ->
-              {next_beams |> Map.update(beam, timelines, fn existing -> existing + timelines end),
-               new_split_count}
+              {next_beams |> Map.update(beam, timelines, &(&1 + timelines)), new_split_count}
 
             "^" ->
               {
                 next_beams
-                |> Map.update(beam - 1, timelines, fn existing -> existing + timelines end)
-                |> Map.update(beam + 1, timelines, fn existing -> existing + timelines end),
+                |> Map.update(beam - 1, timelines, &(&1 + timelines))
+                |> Map.update(beam + 1, timelines, &(&1 + timelines)),
                 new_split_count + 1
               }
           end
         end)
       end
     end)
-    |> elem(0)
-    |> Map.values()
-    |> Enum.sum()
   end
 end
