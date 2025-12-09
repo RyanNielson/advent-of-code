ad391dc252176914d4abfccfa2c7211817aa51f7
Ryan Nielson <ryan.nielson@gmail.com>
2025-12-09 14:30:06 -0400

    Day 8 part 2 and refactor

--- 2025/lib/day_08.ex
+++ 2025/lib/day_08.ex
@@ -1,5 +1,36 @@
 defmodule Day08 do
   def part_1(input, iterations) do
+    {closest_pairs, starting} = prepare(input)
+
+    closest_pairs
+    |> Enum.reduce_while({starting, 0}, fn {p1, p2, _}, {circuits, count} ->
+      new_circuits = connect(p1, p2, circuits)
+
+      count = if circuits != new_circuits, do: count + 1, else: count
+
+      if count == iterations, do: {:halt, new_circuits}, else: {:cont, {new_circuits, count}}
+    end)
+    |> Enum.sort_by(&MapSet.size/1, :desc)
+    |> Enum.take(3)
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.product()
+  end
+
+  def part_2(input) do
+    {closest_pairs, starting} = prepare(input)
+
+    {{x1, _, _}, {x2, _, _}} =
+      closest_pairs
+      |> Enum.reduce_while(starting, fn {p1, p2, _}, circuits ->
+        new_circuits = connect(p1, p2, circuits)
+
+        if length(new_circuits) == 1, do: {:halt, {p1, p2}}, else: {:cont, new_circuits}
+      end)
+
+    x1 * x2
+  end
+
+  defp prepare(input) do
     positions =
       input
       |> String.split()
@@ -29,56 +60,35 @@ defmodule Day08 do
       |> elem(0)
       |> Enum.sort(fn {_, _, d1}, {_, _, d2} -> d1 < d2 end)
 
-    starting = closest_pairs |> Enum.map(fn {position1, _, _} -> MapSet.new([position1]) end)
+    starting = positions |> Enum.map(fn position -> MapSet.new([position]) end)
 
-    closest_pairs
-    |> Enum.reduce_while({starting, 0}, fn {position1, position2, _}, {circuits, count} ->
-      found_1 = Enum.find(circuits, &MapSet.member?(&1, position1))
-      found_2 = Enum.find(circuits, &MapSet.member?(&1, position2))
-
-      new_circuits =
-        cond do
-          found_1 != nil and found_2 != nil ->
-            new_circuits =
-              circuits
-              |> List.delete(found_1)
-              |> List.delete(found_2)
-
-            [MapSet.union(found_1, found_2) | new_circuits]
-
-          found_1 != nil ->
-            new_circuits = circuits |> List.delete(found_1)
-            [MapSet.put(found_1, position2) | new_circuits]
-
-          found_2 != nil ->
-            new_circuits = circuits |> List.delete(found_2)
-            [MapSet.put(found_2, position1) | new_circuits]
-
-          true ->
-            [MapSet.new([position1, position2]) | circuits]
-        end
-
-      count =
-        if circuits != new_circuits do
-          count + 1
-        else
-          count
-        end
-
-      if count == iterations do
-        {:halt, new_circuits}
-      else
-        {:cont, {new_circuits, count}}
-      end
-    end)
-    |> Enum.sort_by(&MapSet.size/1, :desc)
-    |> Enum.take(3)
-    |> Enum.map(&MapSet.size/1)
-    |> Enum.product()
+    {closest_pairs, starting}
   end
 
-  def part_2(input) do
-    input
+  defp connect(p1, p2, circuits) do
+    found_1 = Enum.find(circuits, &MapSet.member?(&1, p1))
+    found_2 = Enum.find(circuits, &MapSet.member?(&1, p2))
+
+    cond do
+      found_1 != nil and found_2 != nil ->
+        new_circuits =
+          circuits
+          |> List.delete(found_1)
+          |> List.delete(found_2)
+
+        [MapSet.union(found_1, found_2) | new_circuits]
+
+      found_1 != nil ->
+        new_circuits = circuits |> List.delete(found_1)
+        [MapSet.put(found_1, p2) | new_circuits]
+
+      found_2 != nil ->
+        new_circuits = circuits |> List.delete(found_2)
+        [MapSet.put(found_2, p1) | new_circuits]
+
+      true ->
+        [MapSet.new([p1, p2]) | circuits]
+    end
   end
 
   defp distance({x1, y1, z1}, {x2, y2, z2}) do
--- 2025/test/day_08_test.exs
+++ 2025/test/day_08_test.exs
@@ -8,8 +8,8 @@ defmodule Day07Test do
     assert Day08.part_1(input("day_08"), 1000) == 75680
   end
 
-  # test "part2" do
-  #   assert Day08.part_2(input("day_07_example_1")) == 40
-  #   assert Day08.part_2(input("day_07")) == 15_650_261_281_478
-  # end
+  test "part2" do
+    assert Day08.part_2(input("day_08_example_1")) == 25272
+    assert Day08.part_2(input("day_08")) == 8_995_844_880
+  end
 end
